7f6dac000063f2d087b235c058ab18506ece2943
Jonny Graybill <jon.a.graybill@gmail.com>
2019-07-09 23:25:26 -0700

    almost through the tests

--- data-modeling/__tests__/models-singular/categories.test.js
+++ data-modeling/__tests__/models-singular/categories.test.js
@@ -8,13 +8,38 @@ describe('Categories Model (Singular)', () => {
   // How will you handle both the happy path and edge cases in these tests?
 
   it('can create() a new category', () => {
+    let obj = { name: 'Video Games', description: 'Is the best category ever' };
+    return categories.create(obj)
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      });
   });
 
   it('can get() a category', () => {
+    let obj = { name: 'Video Games', description: 'We can get this awesome category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[key]).toEqual(obj[key]);
+            });
+          });
+      });
   });
 
   it('can get() all categories', () => {
-  });
+    record => {
+        return categories.get(record._id)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[key]).toEqual(obj[key]);
+            });
+          });
+  };
+});
 
   it('can update() a category', () => {
   });
--- data-modeling/models-singular/categories.js
+++ data-modeling/models-singular/categories.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// Where is our schema defined?
-// How do we get it in here so we can run methods on it?
+const schema = require('./categories-schema.js');
+
 
 class Categories {
 
@@ -9,6 +9,11 @@ class Categories {
   }
 
   get(_id) {
+    if (_id) {
+      return schema.findOne({ _id });
+    } else {
+      return schema.find([{}, {}]);
+    }
     // Call the appropriate mongoose method to get
     // one or more records
     // If 1, return it as a plain object
@@ -16,16 +21,18 @@ class Categories {
     // { count: ##, results: [{}, {}] }
   }
 
+
   create(record) {
-    // Call the appropriate mongoose method to create a new record
+    let newRecord = new schema(record);
+    return newRecord.save();
   }
 
   update(_id, record) {
-    // Call the appropriate mongoose method to update a record
+    return schema.findByIdAndUpdate(_id, record, { new: true });
   }
 
   delete(_id) {
-    // Call the appropriate mongoose method to delete a record
+    return schema.findByIdAndDelete(_id);
   }
 
 }
